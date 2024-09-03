75fffc168d0670905c0e1d89084873ae95bc889f
IvanChi <ichirkov95@yandex.ru>
2024-09-03 18:27:59 +0400

    Complited Work1

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -4,3 +4,21 @@
 // делится ли первое число на второе. Если делится, выводите "делится", иначе
 // выводите "не делится".
 
+Console.WriteLine("Введите первое целое число:");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число:");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+if (secondDigit == 0)
+{
+    Console.WriteLine("Делить на ноль нельзя!");
+}
+else if (firstDigit % secondDigit == 0)
+{
+    Console.WriteLine("Делится!");
+}
+else
+{
+    Console.WriteLine("Не делится!");
+}
