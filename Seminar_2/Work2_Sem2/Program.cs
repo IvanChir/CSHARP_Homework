fa87b8af9c8726806c3e385ed8fd9247432be603
IvanChi <ichirkov95@yandex.ru>
2024-09-09 11:39:37 +0400

    Addet Work1-4_Sem2

---
+++ Seminar_2/Work2_Sem2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 2: 
+// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    {
+        Console.Write("Введите координату X: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите координату Y: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine("Первая координатная четверть");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("Вторая координатная четверть");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("Третья координатная четверть");
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine("Четвертая координатная четверть");
+        }
+        else 
+        {
+            Console.WriteLine("Точка лежит на координатной линии");
+        }
+    }
+}
---
+++ Seminar_2/Work3_Sem2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 3: 
+// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    {
+        Console.Write("Введите число от 10 до 99: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        
+        if (num >= 10 && num <= 99)
+        {
+            int firstDigit = num / 10;
+            int secondDigit = num % 10;
+            
+            if (firstDigit > secondDigit)
+            {
+                Console.WriteLine(firstDigit);
+            }
+            else
+            {
+                Console.WriteLine(secondDigit);
+            }
+        
+        }
+        else 
+        {
+            Console.WriteLine("Введеное число не входит в отрезок [10, 99]");
+        }
+    }
+}
