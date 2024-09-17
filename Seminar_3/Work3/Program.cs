a003b99b0cdf40a3fdda7299f2751b42fce46bd9
IvanChi <ichirkov95@yandex.ru>
2024-09-17 19:58:37 +0400

    Addet Homework Seminsr_3

---
+++ Seminar_3/Work3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задание 3: Поиск среднего значения массива
+// Описание: Задайте массив из 10 целых чисел. Найдите среднее значение элементов массива.
+
+
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 8 };
+
+int i = 0;
+double sum = 0;
+double average = 0;
+
+while (i < array.Length)
+{
+    sum = sum + array[i];
+    i++;
+}
+average = sum / array.Length;
+
+Console.WriteLine(average);
---
+++ Seminar_3/Work4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задание 4: Удаление всех отрицательных чисел
+// Описание: Задайте массив целых чисел. Напишите программу, которая удаляет все
+// отрицательные числа из массива и возвращает новый массив, содержащий только
+// неотрицательные числа.
+
+
+
+int[] arr = { -1, 2, -3, 4, -5, 6 };
+int count = 0;
+
+for(int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] >= 0)
+    {
+        count++;
+    }
+}
+
+int[] nonNegativeNumbers = new int[count];
+int index = 0;
+
+for(int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] >= 0)
+    {
+        nonNegativeNumbers[index] = arr[i];
+        index++;
+    }
+}
+
+for (int i = 0; i < nonNegativeNumbers.Length; i++)
+{
+    Console.Write(nonNegativeNumbers[i] + ", ");
+}
+
